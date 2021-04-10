3c91cab97764fa3fa48eb5d2a10976ca0ada9aee
wrightben <mail@wrightben.com>
2021-04-10 01:07:11 -0400

    gitall commit

--- Poker/Auto/Insights/scenarios/3h3d - Example/Scenario/poker.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-COUNTER=1000
-until [  $COUNTER -lt 1 ]; do
-	let COUNTER-=1
-	echo COUNTER $COUNTER
-	node ../texas-holdem.js  >> hands.txt
-done
---
+++ Poker/Auto/Insights/scenarios/AhAd - AcKc/README.md
@@ -0,0 +1,3 @@
+# Unexpected
+
+AA vs AK suited shows that AA dominates. It might just be sample size (1000 hands). But I was surprised by this result.

067fc809a00d2313eefb1ba2f055494a6940d28c
wrightben <mail@wrightben.com>
2021-04-10 20:04:48 -0400

    gitall commit

--- Poker/Auto/Scenario/scenario.sh
+++ Poker/Auto/Scenario/scenario.sh
@@ -7,4 +7,12 @@ sh ./poker.sh
 if test -f "../distribution.csv"; then
    rm ../distribution.csv
 fi
+
+# Analysis
 php analysis.php
+cd "Optional"
+php position.php
+
+# Optional position distribution
+#php position.php 2
+#php position.php 3
